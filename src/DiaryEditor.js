04e896a8768007e468dca14dda1290a153d506c5
seeseal <cur12151@naver.com>
2022-04-20 10:36:33 +0900

    DOM 조작하기

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -15,8 +18,18 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
+
     console.log(state);
-    alert("저장 성공");
+    alert("저장 성공!");
   };
 
   return (
@@ -24,16 +37,22 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
-          name="author"
+          ref={authorInput}
           value={state.author}
           onChange={handleChangeState}
+          name="author"
+          placeholder="작성자"
+          type="text"
         />
       </div>
       <div>
         <textarea
-          name="content"
+          ref={contentInput}
           value={state.content}
           onChange={handleChangeState}
+          name="content"
+          placeholder="일기"
+          type="text"
         />
       </div>
       <div>
